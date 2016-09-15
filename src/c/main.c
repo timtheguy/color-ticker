1702de0772dd5e442b88f1f6c7d2188f8802268d
Tim Christovich <christ10@my.erau.edu>
2016-09-15 18:06:26 -0400

    Updated code layout

--- src/c/main.c
+++ src/c/main.c
@@ -7,14 +7,9 @@ static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_hexcolor_layer;
 
-static BitmapLayer *lightest_layer;
-static BitmapLayer *lighter_layer;
 static BitmapLayer *color_layer;
-static BitmapLayer *darker_layer;
-static BitmapLayer *darkest_layer;
 
-
-unsigned long hex2int(char *a, unsigned int len)
+unsigned long hex2int(char *a, unsigned int len) //converts hexadecimal number to integer
 {
     unsigned int i;
     unsigned long val = 0;
@@ -28,7 +23,7 @@ unsigned long hex2int(char *a, unsigned int len)
     return val;
 }
 
-char* concat(char *s1, char *s2, char *s3) //ALLOWS CONCATONATION
+char* concat(char *s1, char *s2, char *s3) //function to concatonate strings
 {
     char *result = malloc(strlen(s1)+strlen(s2)+strlen(s3)+1);
     strcpy(result, s1);
@@ -37,88 +32,58 @@ char* concat(char *s1, char *s2, char *s3) //ALLOWS CONCATONATION
     return result;
 }
 
-static void update_background(){
+static void update_background(){ //updates the background of the watch and sets the color
   char str[STR_LEN + 1] = {0};
   const char *hex_digits = "0123456789ABCDEF";
   int i;
 
-  for( i = 0 ; i < STR_LEN; i++ ) {
+  for( i = 0 ; i < STR_LEN; i++ ) { 
      str[i] = hex_digits[ ( rand() % 16 ) ];
   }
   
   char* s_hexcolor = "#000000";
   s_hexcolor = concat("#", str, "");
-  APP_LOG(APP_LOG_LEVEL_DEBUG, str);
-  APP_LOG(APP_LOG_LEVEL_DEBUG, s_hexcolor);
-  text_layer_set_text(s_hexcolor_layer, s_hexcolor);
+
+  text_layer_set_text(s_hexcolor_layer, s_hexcolor); //set text to random color
   
+  int bg_color = hex2int(str, 6); //get the background color
 
-  //text_layer_set_text(s_hexcolor_layer, str);
-  
-  int bg_color = hex2int(str, 6);
-  
-//   int bg_color1 = (((bg_color & 0xfefefe) >> 1));
-//   int bg_color2 = (((bg_color & 0xfefefe) << 1));
-  
-//   GColor gColorLightest = GColorFromHEX(bg_color1);
-//   GColor gColorLighter = GColorFromHEX(bg_color1);
-//   GColor gColorNormal = GColorFromHEX(bg_color);
-//   GColor gColorDarker = GColorFromHEX(bg_color2);
-//   GColor gColorDarkest = GColorFromHEX(bg_color2);
-  
-//   bitmap_layer_set_background_color(lightest_layer, (gColorLightest));
-//   bitmap_layer_set_background_color(lighter_layer, (gColorLighter));
-//     bitmap_layer_set_background_color(darker_layer, (gColorDarker));
-//   bitmap_layer_set_background_color(darkest_layer, (gColorDarkest));
-
-
-
-    
-  window_set_background_color(s_main_window, GColorFromHEX(bg_color));
-  text_layer_set_text_color(s_time_layer, gcolor_legible_over(GColorFromHEX(bg_color)));
-  text_layer_set_text_color(s_hexcolor_layer, gcolor_legible_over(GColorFromHEX(bg_color)));
-
-
+  window_set_background_color(s_main_window, GColorFromHEX(bg_color)); //set the background color
+  text_layer_set_text_color(s_time_layer, gcolor_legible_over(GColorFromHEX(bg_color))); //set the color of text to be legible
+  text_layer_set_text_color(s_hexcolor_layer, gcolor_legible_over(GColorFromHEX(bg_color))); //set the color of text to be legible
 }
 
 static void update_time() {
-  // Get a tm structure
   time_t temp = time(NULL); 
-  struct tm *tick_time = localtime(&temp);
+  struct tm *tick_time = localtime(&temp); //get a time structure setup
 
-  // Write the current hours and minutes into a buffer
-  static char s_buffer[8];
+  static char s_buffer[8]; //write current hours and minutes into buffer
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
                                           "%H:%M" : "%l:%M", tick_time);
 
-  // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, s_buffer);
+  text_layer_set_text(s_time_layer, s_buffer); //display the time on the textlayer
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
   update_background();
+  update_time();
+  
 }
 
 static void main_window_load(Window *window) {
-    // Get information about the Window
-  Layer *window_layer = window_get_root_layer(window);
+  Layer *window_layer = window_get_root_layer(window); //get window information
   GRect bounds = layer_get_bounds(window_layer);
 
-  // Create the TextLayer with specific bounds
-  s_time_layer = text_layer_create(
-      //starting x, starting y, width x, width y
-      GRect(0, PBL_IF_ROUND_ELSE(48, 40), bounds.size.w, 50));
+  s_time_layer = text_layer_create( //create the TextLayer with specific bounds
+      GRect(0, PBL_IF_ROUND_ELSE(48, 40), bounds.size.w, 50)); //starting x, starting y, width x, width y
 
-  //Create the TextLayer for Text
-  s_hexcolor_layer = text_layer_create(
-      //starting x, starting y, width x, width y
-      GRect(0, PBL_IF_ROUND_ELSE(88, 80), bounds.size.w, 50));
   
-  // Improve the layout to be more like a watchface
+  s_hexcolor_layer = text_layer_create( //create the TextLayer for Text
+      GRect(0, PBL_IF_ROUND_ELSE(88, 80), bounds.size.w, 50)); //starting x, starting y, width x, width y
+ 
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "0:00");
+  text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   
@@ -128,82 +93,36 @@ static void main_window_load(Window *window) {
   text_layer_set_font(s_hexcolor_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28) );
   text_layer_set_text_alignment(s_hexcolor_layer, GTextAlignmentCenter);
 
-//   double x = 0;
-//   lightest_layer = bitmap_layer_create(
-//       //starting x, starting y, width x, width y
-//       GRect(x, 108, (bounds.size.w/4), 60));
   
-//   x+= bounds.size.w/4;
-//   lighter_layer = bitmap_layer_create(
-//       //starting x, starting y, width x, width y
-//       GRect(x, 108, (bounds.size.w/4), 60));
-  
-//   x+= bounds.size.w/4;
-//   darker_layer = bitmap_layer_create(
-//       //starting x, starting y, width x, width y
-//       GRect(x, 108, (bounds.size.w/4), 60));
-  
-//   x+= bounds.size.w/4;
-//   darkest_layer = bitmap_layer_create(
-//       //starting x, starting y, width x, width y
-//       GRect(x, 108, (bounds.size.w/4), 60));
-  
-  
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer, text_layer_get_layer(s_hexcolor_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_hexcolor_layer)); //add it as a child layer to the Window's root layer
 
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
-  
-//   layer_add_child(window_layer, bitmap_layer_get_layer(lightest_layer));
-//   layer_add_child(window_layer, bitmap_layer_get_layer(lighter_layer));
-//   layer_add_child(window_layer, bitmap_layer_get_layer(darker_layer));
-//   layer_add_child(window_layer, bitmap_layer_get_layer(darkest_layer));
-  
-//   bitmap_layer_set_background_color(lightest_layer, GColorBlack);
-//   bitmap_layer_set_background_color(lighter_layer, GColorBlack);
-//   bitmap_layer_set_background_color(darker_layer, GColorBlack);
-//   bitmap_layer_set_background_color(darkest_layer, GColorBlack);
-  
- 
-
 }
 
 static void main_window_unload(Window *window) {
-  // Destroy TextLayer
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_hexcolor_layer);
-  bitmap_layer_destroy(lightest_layer);
-  bitmap_layer_destroy(lighter_layer);
   bitmap_layer_destroy(color_layer);
-  bitmap_layer_destroy(darker_layer);
-  bitmap_layer_destroy(darkest_layer);
-
 }
 
 
 static void init() {
-  // Create main Window element and assign to pointer
-  s_main_window = window_create();
+  s_main_window = window_create();   //create main Window element and assign to pointer
 
-  // Set handlers to manage the elements inside the Window
-  window_set_window_handlers(s_main_window, (WindowHandlers) {
+  window_set_window_handlers(s_main_window, (WindowHandlers) { //set handlers to manage the elements inside the Window
     .load = main_window_load,
     .unload = main_window_unload
   });
 
-  // Show the Window on the watch, with animated=true
-  window_stack_push(s_main_window, true);
+  window_stack_push(s_main_window, true); //show the Window on the watch, with animated=true
 
-  // Make sure the time is displayed from the start
-  update_time();
+  update_time(); //make sure the time is displayed from the start
 
-  // Register with TickTimerService
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler); //register with TickTimerService
 }
 
 static void deinit() {
-  // Destroy Window
-  window_destroy(s_main_window);
+  window_destroy(s_main_window); //destroy Window
 }
 
 int main(void) {
